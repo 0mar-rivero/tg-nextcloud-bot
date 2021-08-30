150e36bab4ac5dd8ebe1953468e9517112e64a6c
Omar Rivero <omarrivgw@gmail.com>
2021-08-29 23:38:30 -0400

    separando las descargas de las subidas

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
         os.makedirs(downloads_path, exist_ok=True)
         if filename in os.listdir(downloads_path):
             await reply.edit(f'{filename} already downloaded')
-            return downloads_path.joinpath(filename)
+            return downloads_path.joinpath('/' + filename)
         else:
             await reply.edit(f'{filename} being downloaded')
 
